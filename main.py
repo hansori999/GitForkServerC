6f2986709dea232d318c7bd6ed281175a0da0af1
Myoung-Wan Koo <mwkoo9@gmail.com>
2020-09-25 14:55:35 +0900

    Hello World 3

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ print("Hello World 1 in team leader")
 
 print("Hello World 2 in team leader")
 
-print("Hello World 3 in local PC of team member")
+print("Hello World 3 in local PC of team member"))
